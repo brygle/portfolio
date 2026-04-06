7118d179af0d17911bafa7bc120ad3e7fa040142
Lopez Echeverria, Bryan Gustavo <bglopez@bi.com.gt>
2026-04-06 13:36:31 -0600

    adding a local image

--- src/app/components/Sections/About.tsx
+++ src/app/components/Sections/About.tsx
@@ -5,6 +5,7 @@ import HeaderText from "../Text/HeaderText";
 import TitleText from "../Text/TitleText";
 import Text from "../Text/Text";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
 
 export default function About() {
 
@@ -16,12 +17,13 @@ export default function About() {
                 <HeaderText className="text-center">{t('about.introduce')}</HeaderText>
                 <TitleText className="text-center">{t('about.welcome')}</TitleText>
                 <div className="flex flex-col w-full gap-[16px] justify-center items-center md:gap-[64px] md:flex-row">
-                    <img 
-                        src='https://media.licdn.com/dms/image/v2/C4E03AQGnc9O4YEXBrA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1633898244470?e=1757548800&v=beta&t=H5H05tUDGUVi0GiyQdvOpnRY_OFdZZBjsuhXczNMk6g'
-                        width="250"
-                        height="250"
+                    <Image 
+                        src="/bryan.jfif"
+                        alt="Me"
+                        width={250}
+                        height={250}
                         className="w-[250px] h-[250px] rounded-full"
-                        alt="Me" 
+                        unoptimized 
                     />
                     <div className="flex flex-col gap-[4px] md:items-start">
                         <Text className="text-center">{t('about.name')}</Text>
