e8f7c1bc0e6e393ad2b000f4d270a9b4386c10e4
brygle <bryansyng2@gmail.com>
2026-09-22 21:21:30 -0600

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import About from './components/Sections/About';
-import Studies from './components/Sections/Studies';
 import Skills from './components/Sections/Skills';
 import Experience from './components/Sections/Experience';
-import Projects from './components/Sections/Projects';
 
 export default function Home() {
   return (
