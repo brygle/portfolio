c7e2f19387cc46c4d9234c86e1179fb8ef11026a
brygle <bryansyng2@gmail.com>
2026-09-22 19:39:49 -0600

    adding icon test

--- src/app/components/Sections/Skills.tsx
+++ src/app/components/Sections/Skills.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import Section from "../Layout/Section";
+import Image from "next/image";
 
 export default function Skills() {
-
+  
   return (
     <Section id='skills'>
         <div>Skills</div>
+        <Image
+          src="/icons/css.png"
+          alt="CSS"
+          width={48}
+          height={48}
+        />
     </Section>
   );
 }
